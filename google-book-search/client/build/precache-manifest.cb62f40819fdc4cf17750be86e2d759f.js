e46bf9972cd60fffce63eef3422f0702dc3b5d0d
bricezakra <bricezakra@live.com>
2019-11-06 07:38:15 -0600

    routes, models, controllers and scripts folders set up

---
+++ google-book-search/client/build/precache-manifest.cb62f40819fdc4cf17750be86e2d759f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca83cfbdd147775fdba990931d0be37e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b1d57081efb5ce8411d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "716c617225afef6ea003",
+    "url": "/static/js/2.9a0ce7d2.chunk.js"
+  },
+  {
+    "revision": "7b1d57081efb5ce8411d",
+    "url": "/static/js/main.0c4f32f0.chunk.js"
+  },
+  {
+    "revision": "06901b8cdf4db76dca8f",
+    "url": "/static/js/runtime-main.13ef9398.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
